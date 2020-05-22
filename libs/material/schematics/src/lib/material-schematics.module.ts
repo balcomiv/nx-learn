aa407320a1515eb8953d34dca0939e94975c6000
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-05-22 16:50:13 -0400

    Removing some libs

--- libs/material/schematics/src/lib/material-schematics.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SmokeTestComponent } from './ui/smoke-test/smoke-test.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [SmokeTestComponent]
-})
-export class MaterialSchematicsModule {}
