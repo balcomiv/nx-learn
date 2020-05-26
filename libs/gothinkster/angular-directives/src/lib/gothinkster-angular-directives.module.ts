a6bd910f6d8e1e3f3a0c8ff033cfffe9dc53a3da
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-05-26 10:03:53 -0400

    Add - Component - Person

--- libs/gothinkster/angular-directives/src/lib/gothinkster-angular-directives.module.ts
+++ libs/gothinkster/angular-directives/src/lib/gothinkster-angular-directives.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SmokeTestComponent } from './ui/smoke-test/smoke-test.component';
 import { DefinitionDirective } from './directives/definition.directive';
+import { PersonComponent } from './ui/person/person.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SmokeTestComponent, DefinitionDirective]
+  declarations: [SmokeTestComponent, DefinitionDirective, PersonComponent]
 })
 export class GothinksterAngularDirectivesModule {}
