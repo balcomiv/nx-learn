e7cc9637f95501553cb9b08d93de9c1b09ba8682
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-05-27 08:18:53 -0400

    Add - Directive - BtnGrow

--- libs/gothinkster/angular-directives/README.md
+++ libs/gothinkster/angular-directives/README.md
@@ -11,3 +11,9 @@ Run `nx test gothinkster-angular-directives` to execute the unit tests.
 ### Multiple, Named Inputs on directive
 
 - https://stackoverflow.com/questions/38843532/how-to-pass-multiple-parameter-to-directives-components-in-angular-with-type
+
+### Thinkster Stackblitz Exercises
+
+- https://stackblitz.com/edit/thinkster-directives-03-03-elementref-exercise-yshzys?file=src%2Fapp%2Fapp.component.ts
+
+- https://stackblitz.com/edit/thinkster-directives-03-05-renderer2-exercise-vkmpkp?file=src%2Fapp%2Fdirectives%2Fcolor.directive.ts
--- libs/gothinkster/angular-directives/src/lib/gothinkster-angular-directives.module.ts
+++ libs/gothinkster/angular-directives/src/lib/gothinkster-angular-directives.module.ts
@@ -4,9 +4,10 @@ import { SmokeTestComponent } from './ui/smoke-test/smoke-test.component';
 import { DefinitionDirective } from './directives/definition.directive';
 import { PersonComponent } from './ui/person/person.component';
 import { HelloWorldDirective } from './directives/hello-world/hello-world.directive';
+import { BtnGrowDirective } from './directives/btn-grow/btn-grow.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SmokeTestComponent, DefinitionDirective, PersonComponent, HelloWorldDirective]
+  declarations: [SmokeTestComponent, DefinitionDirective, PersonComponent, HelloWorldDirective, BtnGrowDirective]
 })
 export class GothinksterAngularDirectivesModule {}
